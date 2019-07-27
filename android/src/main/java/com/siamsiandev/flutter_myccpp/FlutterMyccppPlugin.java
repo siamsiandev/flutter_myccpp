257a27d7b786fe460b638c6c47fd6676d1dae753
bestzillar <kazixma@gmail.com>
2019-07-27 23:16:04 +0700

    add support androidx

--- android/src/main/java/com/siamsiandev/flutter_myccpp/FlutterMyccppPlugin.java
+++ android/src/main/java/com/siamsiandev/flutter_myccpp/FlutterMyccppPlugin.java
@@ -2,7 +2,7 @@ package com.siamsiandev.flutter_myccpp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import io.flutter.plugin.common.MethodCall;
